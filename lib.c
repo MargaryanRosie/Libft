5e94cddd022be6436aee25cd8a23c290b593cc43
Rosie <margaryanrosie@gmail.com>
2025-02-05 11:25:17 +0400

    new commit

---
+++ lib.c
@@ -0,0 +1,584 @@
+static int	ft_get_size(int n)
+{
+	int	size;
+
+	size = 0;
+	if (n <= 0)
+		size++;
+	while (n != 0)
+	{
+		n = n / 10;
+		size++;
+	}
+	return (size);
+}
+
+static void	ft_fill_res(int size, int offset, int n, char *res)
+{
+	while (size > offset)
+	{
+		res[size - 1] = n % 10 + '0';
+		n = n / 10;
+		size--;
+	}
+}
+
+char	*ft_itoa(int n)
+{
+	int		offset;
+	int		size;
+	char	*res;
+
+	offset = 0;
+	size = ft_get_size(n);
+	res = (char *)malloc(sizeof(char) * size + 1);
+	if (!res)
+		return (0);
+	if (n == -2147483648)
+	{
+		res[0] = '-';
+		res[1] = '2';
+		n = 147483648;
+		offset = 2;
+	}
+	if (n < 0)
+	{
+		res[0] = '-';
+		offset = 1;
+		n = -n;
+	}
+	ft_fill_res(size, offset, n, res);
+	res[size] = '\0';
+	return (res);
+}
+
+/*int main()
+{
+	int	i = 125;
+	int	j = -814;
+	int	min = -2147483648;
+	int	max = 2147483647;
+	ft_putstr_fd(ft_itoa(i), 1);
+	ft_putchar_fd('\n', 1);
+	ft_putstr_fd(ft_itoa(j), 1);
+	ft_putchar_fd('\n', 1);
+	ft_putstr_fd(ft_itoa(min), 1);
+	ft_putchar_fd('\n', 1);
+	ft_putstr_fd(ft_itoa(max), 1);
+	ft_putchar_fd('\n', 1);
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*tmp;
+
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	tmp = *lst;
+	while (tmp -> next)
+		tmp = tmp -> next;
+	tmp -> next = new;
+}
+
+
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (lst == NULL || new == NULL)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
+
+
+
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*head;
+	t_list	*tmp;
+
+	if (lst == NULL || del == NULL)
+	{
+		return ;
+	}
+	head = *lst;
+	while (head)
+	{
+		tmp = head -> next;
+		(*del)(head -> content);
+		free(head);
+		head = tmp;
+	}
+	*lst = NULL;
+}
+
+
+
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+	if (lst == NULL || del == NULL)
+		return ;
+	(*del)(lst -> content);
+	free(lst);
+}
+
+
+
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	while (lst)
+	{
+		(*f)(lst -> content);
+		lst = lst -> next;
+	}
+}
+
+
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (lst == NULL)
+		return (NULL);
+	while (lst -> next)
+		lst = lst -> next;
+	return (lst);
+}
+
+
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*head;
+	t_list	*tmp;
+
+	head = NULL;
+	while (lst)
+	{
+		tmp = ft_lstnew((*f)(lst -> content));
+		if (!tmp)
+		{
+			ft_lstclear(&head, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&head, tmp);
+		lst = lst -> next;
+	}
+	return (head);
+}
+
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (new == NULL)
+	{
+		return (0);
+	}
+	new -> content = content;
+	new -> next = NULL;
+	return (new);
+}
+
+/*int	main(void)
+{
+	char	*str;
+
+	t_list	*elem;
+	str = (char *)malloc(6*sizeof(char));
+	str = "Hello";
+	elem = ft_lstnew((void *)str);
+	printf("%s\n", (char *)elem->content);
+	free(new->content);
+}*/
+
+
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int	i;
+
+	i = 0;
+	while (lst)
+	{
+		lst = lst-> next;
+		++i;
+	}
+	return (i);
+}
+
+
+#include "libft.h"
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+
+
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	ft_putstr_fd(s, fd);
+	write(fd, "\n", 1);
+}
+
+
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n < 0)
+	{
+		if (n == -2147483648)
+		{
+			ft_putstr_fd("-2147483648", fd);
+			return ;
+		}
+		ft_putchar_fd('-', fd);
+		n = -n;
+	}
+	if (n / 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+		ft_putchar_fd('0' + n, fd);
+}
+
+
+#include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s)
+		return ;
+	write(fd, s, ft_strlen(s));
+}
+
+
+
+
+
+
+static void	ft_allocate(char **tab, char const *s, char sep)
+{
+	char		**tab1;
+	char const	*tmp;
+
+	tmp = s;
+	tab1 = tab;
+	while (*tmp)
+	{
+		while (*s == sep)
+			++s;
+		tmp = s;
+		while (*tmp && *tmp != sep)
+			++tmp;
+		if (*tmp == sep || tmp > s)
+		{
+			*tab1 = ft_substr(s, 0, tmp - s);
+			s = tmp;
+			++tab1;
+		}
+	}
+	*tab1 = NULL;
+}
+
+static int	ft_count_words(char const *s, char sep)
+{
+	int	word_count;
+
+	word_count = 0;
+	while (*s)
+	{
+		while (*s == sep)
+			++s;
+		if (*s)
+			++word_count;
+		while (*s && *s != sep)
+			++s;
+	}
+	return (word_count);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**new;
+	int		size;
+
+	if (!s)
+		return (NULL);
+	size = ft_count_words(s, c);
+	new = (char **)malloc(sizeof(char *) * (size + 1));
+	if (!new)
+		return (NULL);
+	ft_allocate(new, s, c);
+	return (new);
+}
+
+/*int	main()
+{
+	char	str[] = "Eu vou ser splitado!";
+	char	c = ' ';
+	char	**spt = ft_split(str, c);
+	int	i = 0;
+	while (spt[i] != 0)
+	{
+		ft_putstr_fd(spt[i], 1);
+		i++;
+	}
+	ft_putchar_fd('\n', 1);
+}*/
+
+
+
+
+
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	unsigned int	i;
+
+	i = 0;
+	if (!s)
+		return ;
+	while (s[i])
+	{
+		(*f)(i, (s + i));
+		++i;
+	}
+}
+
+/*void	ft_test(unsigned int i, char *str)
+	{
+		*str += i;
+	}
+int	main()
+{
+	char	str[] = "aBcDeF";
+	ft_striteri(str, ft_test);
+	ft_putendl_fd(str, 1);
+}*/
+
+
+
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dest;
+	size_t	len1;
+	size_t	len2;
+
+	if (!s1 && !s2)
+		return (ft_strdup(""));
+	if (s1 && !s2)
+		return (ft_strdup(s1));
+	if (!s1 && s2)
+		return (ft_strdup(s2));
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	dest = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!dest)
+		return (NULL);
+	ft_strlcpy(dest, s1, len1 + 1);
+	ft_strlcat(dest, s2, len1 + len2 + 1);
+	return (dest);
+}
+
+/*int	main()
+{
+	char	str[] = "Junta te a mim, ";
+	char	str1[] = "nos ja vivemos 100 mil anos!";
+	ft_putendl_fd(ft_strjoin(str, str1), 1);
+}*/
+
+
+
+
+
+#include "libft.h"
+//#include <stdio.h>
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	char	*pt_src;
+
+	pt_src = (char *)src;
+	i = 0;
+	while (i < size && *dst)
+	{
+		dst++;
+		i++;
+	}
+	if (i == size)
+		return (i + ft_strlen(src));
+	j = 0;
+	while (pt_src[j])
+	{
+		if (j < size - i - 1)
+			*dst++ = pt_src[j];
+		j++;
+	}
+	*dst = 0;
+	return (j + i);
+}
+
+/*int	main()
+{
+	char	dest[40] = "nem me vais ver!";
+	char	src[] = "nem a mim, mas vais saber o nosso tamanho!";
+	ft_putnbr_fd(ft_strlcat(dest, src, 15), 1);
+	ft_putchar_fd('\n', 1);
+	printf("%s", dest);
+}*/
+
+
+
+
+
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*new;
+	size_t	len;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (ft_strdup(""));
+	len = ft_strlen(s);
+	new = (char *)malloc(sizeof(char) * (len + 1));
+	if (!new)
+		return (NULL);
+	while (i < len)
+	{
+		new[i] = (*f)(i, s[i]);
+		++i;
+	}
+	new[i] = 0;
+	return (new);
+}
+
+/*char	ft_test(unsigned int i, char s)
+{
+	s += i;
+}
+int	main()
+{
+	char	str[] = "Ola";
+	ft_putendl_fd(ft_strmapi(str, ft_test), 1);
+}*/
+
+
+
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*trimmed;
+	size_t	start;
+	size_t	end;
+
+	if (!s1)
+		return (NULL);
+	if (!set)
+		return (ft_strdup(s1));
+	end = ft_strlen(s1) - 1;
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		++start;
+	while (s1[end] && ft_strchr(set, s1[end]))
+		--end;
+	trimmed = ft_substr(s1, start, end - start + 1);
+	return (trimmed);
+}
+
+/*int	main()
+{
+	char	str[] = "Where's Wally?";
+	char	str1[] = "Wally";
+	ft_putendl_fd(ft_strtrim(str, str1), 1);
+}*/
+
+
+
+
+
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*subst;
+	size_t	size;
+
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	size = ft_strlen(s + start);
+	if (size < len)
+		len = size;
+	subst = (char *)malloc(sizeof(char) * (len + 1));
+	if (!subst)
+		return (NULL);
+	ft_strlcpy(subst, s + start, len + 1);
+	return (subst);
+}
+
+/*int	main()
+{
+	char	str[] = "Mais um pouco e era Sub-Zero!";
+	ft_putendl_fd(ft_substr(str, 20, 30), 1);
+}*/
