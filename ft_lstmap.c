0b70acea3dbeba895a06e9146c31f6a0e19d0860
MargaryanRosie <rosie.margaryan@bk.ru>
2025-02-16 14:11:59 +0400

    Add files via upload

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,34 +1,26 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: romargar <romargar@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/15 15:29:53 by romargar          #+#    #+#             */
-/*   Updated: 2025/02/15 16:58:03 by romargar         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft.h"
 #include <stdio.h>
+#include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*head;
-	t_list	*tmp;
+	t_list	*new_list;
+	void	*new_content;
+	t_list	*new_node;
 
-	head = NULL;
+	if (!lst || !f || !del)
+		return (NULL);
+	new_list = NULL;
 	while (lst)
 	{
-		tmp = ft_lstnew((*f)(lst->content));
-		if (!tmp)
+		new_content = f(lst -> content);
+		new_node = ft_lstnew(new_content);
+		if (!new_node)
 		{
-			ft_lstclear(&head, del);
-			return (NULL);
+			ft_lstclear(&new_list, del);
+			break ;
 		}
-		ft_lstadd_back(&head, tmp);
-		lst = lst->next;
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst -> next;
 	}
-	return (head);
+	return (new_list);
 }
